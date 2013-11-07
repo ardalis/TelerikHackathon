506b9ddef211389fe4da291cd3f185e6ebe0f4ed
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2013-11-07 15:01:02 -0500

    Added pie charts and frequent opponent data

--- GameKeeperMobile/scripts/viewModels/userProfileViewModel.js
+++ GameKeeperMobile/scripts/viewModels/userProfileViewModel.js
@@ -8,16 +8,33 @@
         createdAt: new Date(2013, 10, 6),
         recentMatches: 14,
         recentWins: 3,
-        topGames: [
-            {name: "Chess", wins: 10},
-            { name: "Checkers", wins: 8 },
-            { name: "Backgammon", wins: 6 },
-            { name: "Dominoes", wins: 4 },
-            { name: "Settlers of Catan", wins: 3 }
-        ],
-        formattedCreateDate: function() {
+        topGames: new kendo.data.DataSource({
+            data: [
+                { name: "Chess", wins: 10 },
+                { name: "Checkers", wins: 8 },
+                { name: "Backgammon", wins: 6 },
+                { name: "Dominoes", wins: 4 },
+                { name: "Settlers of Catan", wins: 3 }
+            ]
+        }),
+        winLoss: new kendo.data.DataSource({
+            data: [
+                { name: "Wins", count: 20},
+                { name: "Losses", count: 10 }
+            ]
+        }),
+        formattedCreateDate: function () {
             return kendo.toString(vm.createdAt, "MMM d, yyyy");
-        }
+        },
+        frequentOpp: "Mary Contrary",
+        frequentOppWins: 10,
+        frequentOppLosses: 8,
+        winOpp: "Jeff Fritz",
+        winOppWins: 13,
+        winOppLosses: 3,
+        lossOpp: "Phil Japikse",
+        lossOppWins: 4,
+        lossOppLosses: 8
     });
 
     return vm;
