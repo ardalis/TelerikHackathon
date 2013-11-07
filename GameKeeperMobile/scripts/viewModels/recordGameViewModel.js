725c9e78d18e90de69b7d398f9874f838eb50aea
Chris Wagner <Chris.Wagner@telerik.com>
2013-11-07 10:40:14 -0500

    Added dummy events list / filter.

--- GameKeeperMobile/scripts/viewModels/recordGameViewModel.js
+++ GameKeeperMobile/scripts/viewModels/recordGameViewModel.js
@@ -1,6 +1,15 @@
 ﻿define([], function () {
     "use strict";
 
+    var fakeEventsDataSource = new kendo.data.DataSource({
+        data: _.range(1, 12).map(function (i) {
+            return {
+                id: kendo.guid(),
+                title: kendo.format("BOGA {0}/{0}", i)
+            };
+        })
+    });
+
     var vm = kendo.observable({
         selectedEvent: null,
         hasSelectedEvent: function () {
@@ -8,7 +17,9 @@
         },
         selectedEventTitle: function () {
             return vm.hasSelectedEvent() ? vm.get("selectedEvent").title : "";
-        }
+        },
+        eventFilter: "",
+        eventsDataSource: fakeEventsDataSource
     });
     return vm;
 });
