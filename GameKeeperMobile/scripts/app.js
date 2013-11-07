616efb6ca0c80cef8d53a343d3768806b0faa805
Chris Wagner <Chris.Wagner@telerik.com>
2013-11-07 09:10:51 -0500

    Switched to flat theme.

--- GameKeeperMobile/scripts/app.js
+++ GameKeeperMobile/scripts/app.js
@@ -18,18 +18,9 @@ require([], function () {
         navigator.splashscreen.hide();
     }, false);
 
-    app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout", statusBarStyle: statusBarStyle });
-
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
-    };
+    app.application = new kendo.mobile.Application(document.body, {
+        layout: "tabstrip-layout",
+        statusBarStyle: statusBarStyle,
+        skin: "flat"
+    });
 });
