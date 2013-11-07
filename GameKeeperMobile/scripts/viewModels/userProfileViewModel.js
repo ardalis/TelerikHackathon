84755e18f4584cee6d40a6d1e9a0e6eed4b7b926
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2013-11-07 12:31:46 -0500

    Built out first part of the My Profile screen

--- GameKeeperMobile/scripts/viewModels/userProfileViewModel.js
+++ GameKeeperMobile/scripts/viewModels/userProfileViewModel.js
@@ -1,12 +1,25 @@
 ﻿define([], function () {
     "use strict";
 
-    return kendo.observable ({
+    var vm = kendo.observable ({
         id: kendo.guid(),
         name: "Joe Bag O'Donuts",
-        email: "joe.bagodonuts@telerik.com",
-        gravatar: "http://www.gravatar.com/avatar/cb1dd14d527aa08f0d983a3d8feb96cd.png",
-        createdAt: new Date(2013, 10, 6)
+        email: "jeff.fritz@telerik.com",
+        createdAt: new Date(2013, 10, 6),
+        recentMatches: 14,
+        recentWins: 3,
+        topGames: [
+            {name: "Chess", wins: 10},
+            { name: "Checkers", wins: 8 },
+            { name: "Backgammon", wins: 6 },
+            { name: "Dominoes", wins: 4 },
+            { name: "Settlers of Catan", wins: 3 }
+        ],
+        formattedCreateDate: function() {
+            return kendo.toString(vm.createdAt, "MMM d, yyyy");
+        }
     });
+
+    return vm;
 
 });
