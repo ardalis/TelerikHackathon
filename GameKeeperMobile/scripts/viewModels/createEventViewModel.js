508ee4a5d72c84ad3fb14ff03a02d19a452c7a0d
Chris Wagner <Chris.Wagner@telerik.com>
2013-11-07 12:22:49 -0500

    Started adding event creation.

--- GameKeeperMobile/scripts/viewModels/createEventViewModel.js
+++ GameKeeperMobile/scripts/viewModels/createEventViewModel.js
@@ -1,14 +1,33 @@
 ﻿define(["radio"], function (radio) {
     "use strict";
 
+    var validator = $("#create-event-form").kendoValidator().data("kendoValidator");
+
     var vm = kendo.observable({
         title: "",
         location: "",
         date: "",
+
         onUseCurrentLocationTapped: function () {
             navigator.geolocation.getCurrentPosition(function (position) {
                 vm.set("location", kendo.format("{0}, {1}", position.coords.latitude, position.coords.longitude));
             });
+        },
+        onSaveTapped: function (e) {
+            e.preventDefault();
+            if (validator.validate()) {
+                radio("event/created").broadcast({
+                    title: vm.get("title"),
+                    location: vm.get("location"),
+                    date: vm.get("date")
+                });
+                vm.clear();
+            }
+        },
+        clear: function() {
+            vm.set("title", "");
+            vm.set("location", "");
+            vm.set("date", "");
         }
     });
     return vm;
--- GameKeeperMobile/scripts/viewModels/recordGameViewModel.js
+++ GameKeeperMobile/scripts/viewModels/recordGameViewModel.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var fakeEventsDataSource = new kendo.data.DataSource({
-        data: _.range(1, 12).map(function (i) {
+        data: _.range(1, 3).map(function (i) {
             return {
                 id: kendo.guid(),
                 title: kendo.format("BOGA {0}/{0}", i),
@@ -14,22 +14,25 @@
 
     var vm = kendo.observable({
         selectedEvent: null,
+        eventFilter: "",
+        eventsDataSource: fakeEventsDataSource,
+
         hasSelectedEvent: function () {
             return vm.get("selectedEvent") !== null;
         },
         selectedEventTitle: function () {
             return vm.hasSelectedEvent() ? vm.get("selectedEvent").title : "";
         },
-        eventFilter: "",
-        eventsDataSource: fakeEventsDataSource,
+
         onEventSelected: function (e) {
             vm.set("selectedEvent", e.dataItem);
             window.app.application.navigate("#:back");
         }
     });
 
-    radio("event/created").subscribe(function (createEventViewModel) {
-        
+    radio("event/created").subscribe(function (event) {
+        fakeEventsDataSource.add(event);
+        window.app.application.navigate("#:back");
     });
 
     return vm;
