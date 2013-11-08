706721176c2ab57efb0f0d5a21678b146e6eb13e
Chris Wagner <Chris.Wagner@telerik.com>
2013-11-08 08:52:47 -0500

    Added gravatar overrides for judges.

--- GameKeeperMobile/scripts/binders/gravatar.js
+++ GameKeeperMobile/scripts/binders/gravatar.js
@@ -7,13 +7,21 @@
         return kendo.format("http://www.gravatar.com/avatar/{0}?s={1}", emailHash, size);
     };
 
+    // HACK: judges
+    var overrides = {
+        'stephen.forte@telerik.com': '',
+        'mihail.valkov@telerik.com': '',
+        'doug.laird@telerik.com': '',
+        'zarko@telerik.com': ''
+    };
+
     kendo.data.binders.gravatar = kendo.data.Binder.extend({
         refresh: function () {
             var el = $(this.element);
             var email = this.bindings["gravatar"].get() || "";
             var size = parseInt(el.attr("data-gravatar-size"), 10);
 
-            el.attr("src", getImageUrlForEmail(email, size));
+            el.attr("src", overrides[email] || getImageUrlForEmail(email, size));
         }
     });
 });
