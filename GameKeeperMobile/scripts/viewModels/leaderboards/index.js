c34c97c1fbc1e73f1f7370e61ecafee62caddc0e
Chris Wagner <Chris.Wagner@telerik.com>
2013-11-08 08:47:33 -0500

    Factored out common leaderboard view model

--- GameKeeperMobile/scripts/viewModels/leaderboards/index.js
+++ GameKeeperMobile/scripts/viewModels/leaderboards/index.js
@@ -1,5 +1,11 @@
-﻿define(["viewModels/leaderboards/globalWinsViewModel"], function (globalWinsViewModel) {
+﻿define(["viewModels/leaderboards/leaderboardViewModel", "azure-client"], function (leaderboardViewModel, azureClient) {
+    "use strict";
+
+    var globalWins = leaderboardViewModel.create(function() {
+        return azureClient.invokeApi('leaderboardbywins', { method: 'get' });
+    }, 'Wins');
+
     return {
-        globalWins: globalWinsViewModel
+        globalWins: globalWins
     };
 });
---
+++ GameKeeperMobile/scripts/viewModels/leaderboards/leaderboardViewModel.js
@@ -0,0 +1,26 @@
+﻿define([], function () {
+    "use strict";
+
+    var createLeaderboardViewModel = function (createPromise, dimension) {
+        var vm = kendo.observable({
+            rankings: [],
+            dimension: dimension,
+            onBeforeShow: function () {
+                createPromise().done(function (result) {
+                    var wins = JSON.parse(result.response).filter(function (player) {
+                        return player[vm.dimension] > 0;
+                    });
+                    wins.forEach(function (player, i) {
+                        player.Score = player[vm.dimension];
+                        player.Rank = i + 1;
+                    });
+                    vm.set("rankings", wins);
+                });
+            }
+        });
+        return vm;
+    }
+    return {
+        create: createLeaderboardViewModel
+    };
+});
