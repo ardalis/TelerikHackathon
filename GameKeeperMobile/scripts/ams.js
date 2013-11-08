b4bb33f5e644134ff547f261098305ff108872a8
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2013-11-07 21:44:48 -0500

    Began integrating Azure Mobile Services

---
+++ GameKeeperMobile/scripts/ams.js
@@ -0,0 +1,7 @@
+﻿define([], function () {
+    "use strict";
+    return new WindowsAzure.MobileServiceClient(
+        "https://gamekeeper.azure-mobile.net/",
+        "ZgGvfoafTTwUmMfqWXBNnObqSJsfeL31");
+});
+
---
+++ GameKeeperMobile/scripts/datasources.ams.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="ams.js" />
+/// <reference path="MobileServices.Web-1.0.0.min.js" />
+
+define(["ams"], function (ams) {
+    "use strict";
+
+    var createAzureDataSource = function(typeName) {
+        
+        return new kendo.data.DataSource({
+            transport: {
+                read: function(options) {
+                    var ams = new WindowsAzure.MobileServiceClient();
+                    return ams.getTable(typeName);
+                }
+            }
+        })
+
+    }
+
+    var eventsDataSource = createAzureDataSource('Events');
+    var gamesDataSource = createAzureDataSource('Games');
+    var playersDataSource = createAzureDataSource('Players');
+
+    return {
+        events: eventsDataSource,
+        games: gamesDataSource,
+        players: playersDataSource
+    };
+
+
+});
